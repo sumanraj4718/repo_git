ff27882433718c02bc0e23c9e81960e1b45daec7
sumanraj4718 <sumanraj4718@gmail.com>
2025-12-26 13:50:06 +0530

    Add files via upload

---
+++ assignment02.py
@@ -0,0 +1,82 @@
+#ques1.
+#ques2.
+#(i)
+#list=[1,2,3,4,5,]
+# list.append(6)
+# print(list)
+# #(ii)
+# list.insert(2,7)
+# print(list)
+#(iii)
+#list.pop(4)
+#print(list)
+#list.sort(reverse=True)
+#print(list)
+
+##ques3.Showing tuples are immutable.
+# tup=(87,88,89,99)
+# tup[0]=56
+# print(tup)
+#it shows error because it does not support item assignment.
+
+#ques4.
+# A = {1, 2, 3, 4, 5}
+# B = {3, 4, 6, 7}
+# print("Union=",A|B)
+
+# print("Intersection=",A&B)
+# print("Difference=",A-B)
+
+#ques5.
+# Original list with duplicates
+# set1 = [1, 2, 3, 2, 4, 1, 5, 3]
+
+# # Remove duplicates using set
+# set2 = list(set(set1))
+
+# # Print result
+# print("List after removing duplicates:", set2)
+
+#ques6.
+# dict={
+#     "Name":"Suman Raj",
+#     "Roll":"24EC33",
+#     "Marks": 100,
+# }
+#(i)Adding a new key value pair
+# dict["Branch"]="Electronics"
+# print(dict)
+
+#(ii)Updating key and its value.
+#dict["Marks"]=99
+#print(dict)
+
+#(iii)Deleting one key.
+# del dict["Marks"]
+# print(dict)
+
+
+#ques8.
+# text = "Python Data Structures"
+
+# # Convert to lowercase
+# text = text.lower()
+
+# # Replace 'Data' with 'Core'
+# text = text.replace("data", "core")
+
+# # Split into words
+# words = text.split()
+
+# print("Lowercase text:", text)
+# print("Words:", words)
+
+
+#ques9.Print even numbers between 1 to 20.
+# for i in range(2, 21, 2):
+#     print(i)
+
+#ques10.
+# 1. tuples.
+# 2. set.
+# 3. Dictionary.
